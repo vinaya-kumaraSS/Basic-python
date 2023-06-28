a6bb9790581443d46a4542f2d9ec6813aa0bbc24
vinaya-kumaraSS <vinaykumar904@gmail.com>
2023-06-29 02:21:39 +0530

    Added new file

---
+++ operator.py
@@ -0,0 +1,14 @@
+num1=int(input(print("enter the num1:")))
+num2=int(input(print("enter the num2:")))
+add=num1+num2
+sub=num1-num1
+mul=num1*num2
+div=num1/num2
+rem=num1%num2
+pow=num1**num2
+print("addition= ",add)
+print("substraction= ",sub)
+print("multiplication= ",mul)
+print("division= ",div)
+print("reminder= ",rem)
+print("power =",pow)
