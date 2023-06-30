15da227c4a3afef1f68ae11de4affc21c5902cd4
vinaya-kumaraSS <vinaykumarss904@gmail.com>
2023-06-30 20:02:48 +0530

    Added new file

---
+++ for loop.py
@@ -0,0 +1,14 @@
+#example 1
+name="vinay"
+for i in name:
+    print(i)
+#example 2
+colors=["yellow","red","black","green"]
+for i in colors:
+    print(i)
+#example 3
+for k in range(5):
+    print(k)
+#example 4
+for i in range(1,15,4):
+    print(i)
