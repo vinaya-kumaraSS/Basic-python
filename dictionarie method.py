34eb4fb34c833a60c517c7e0d05507dc88a2a228
vinaya-kumaraSS <vinaykumarss904@gmail.com>
2023-07-12 22:23:09 +0530

    Added new file

---
+++ dictionarie method.py
@@ -0,0 +1,13 @@
+student1={1:2,4:5}
+student2={9:5,6:8}
+student1.update(student2)
+print(student1)
+# student1.clear()
+# print(student1)
+student1.pop(1)
+print(student1)
+student1.popitem()
+print(student1)
+name={'name':'vinay','section':'c','number':8861724899}
+del name['section']
+print(name)
